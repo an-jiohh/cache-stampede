bf3b671e29d15df7263090813d7f504c0137f967
an-jiohh <dkswlgh99@naver.com>
2025-12-02 02:06:00 +0900

    feat : Lock을 이용한 Hotkey 문제 해결 테스트

--- src/test/java/jiohh/cachestampede/HotKeyTest.java
+++ src/test/java/jiohh/cachestampede/HotKeyTest.java
@@ -11,18 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CacheStampedTest {
+public class HotKeyTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -48,14 +46,14 @@ public class CacheStampedTest {
     void TTL_만료시_횟수_조회() throws Exception{
         Long itemId = 1L;
         Thread.sleep(12000);
-        
+
         int threads = 100;
         ExecutorService pool = Executors.newFixedThreadPool(threads);
 
         List<Callable<Void>> tasks = new ArrayList<>();
         for (int i = 0; i < threads; i++) {
             tasks.add(() -> {
-                mockMvc.perform(get("/item/{id}", itemId))
+                mockMvc.perform(get("/item/hotSafe/{id}", itemId))
                         .andExpect(status().isOk());
                 return null;
             });
@@ -66,13 +64,94 @@ public class CacheStampedTest {
 
         long cacheMissCounter = itemService.getCacheMissCounter();
         log.info("hot key count : {}", cacheMissCounter);
-        Assertions.assertThat(cacheMissCounter).isNotEqualTo(1);
+        Assertions.assertThat(cacheMissCounter).isEqualTo(1);
     }
 
+    @Test
+    void 캐시_스탬프드_재현() throws Exception {
+        Thread.sleep(1000);
+
+        int requestsPerKey = 10; //1번 동시조회
+        int totalRequests = targetId.size() * requestsPerKey; // 총 100 요청
+        ExecutorService pool = Executors.newFixedThreadPool(totalRequests);
+
+        CountDownLatch ready = new CountDownLatch(totalRequests);
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(totalRequests);
+
+        for (Long id : targetId) {
+            for (int i = 0; i < requestsPerKey; i++) {
+                pool.submit(() -> {
+                    try {
+                        ready.countDown();   // 준비 완료
+                        start.await();       // 모두 준비될 때까지 대기
+                        mockMvc.perform(get("/item/{id}", id))
+                                .andExpect(status().isOk());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        done.countDown();    // 완료 신호
+                    }
+                });
+            }
+        }
+
+        // 모든 스레드 준비될 때까지 기다렸다가 동시에 시작
+        ready.await();
+        start.countDown();
+        done.await();
+
+        pool.shutdown();
+        long cacheMissCounter = itemService.getCacheMissCounter();
+        log.info("basic miss count : {}", cacheMissCounter);
+
+        Assertions.assertThat(cacheMissCounter).isGreaterThanOrEqualTo(3);
+    }
+
+    @Test
+    void 캐시_스탬프드_개선_Jitter() throws Exception {
+        Thread.sleep(1000);
+
+        int requestsPerKey = 10; //1번 동시조회
+        int totalRequests = targetId.size() * requestsPerKey; // 총 100 요청
+        ExecutorService pool = Executors.newFixedThreadPool(totalRequests);
+
+        CountDownLatch ready = new CountDownLatch(totalRequests);
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch done = new CountDownLatch(totalRequests);
+
+        for (Long id : targetId) {
+            for (int i = 0; i < requestsPerKey; i++) {
+                pool.submit(() -> {
+                    try {
+                        ready.countDown();   // 준비 완료
+                        start.await();       // 모두 준비될 때까지 대기
+                        mockMvc.perform(get("/item/hotSafe/{id}", id))
+                                .andExpect(status().isOk());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        done.countDown();    // 완료 신호
+                    }
+                });
+            }
+        }
+
+        // 모든 스레드 준비될 때까지 기다렸다가 동시에 시작
+        ready.await();
+        start.countDown();
+        done.await();
+
+        pool.shutdown();
+        long cacheMissCounter = itemService.getCacheMissCounter();
+        log.info("lock miss count : {}", cacheMissCounter);
+
+        Assertions.assertThat(cacheMissCounter).isGreaterThanOrEqualTo(3);
+    }
 
     @Test
     void 캐시_스탬피드_지속_재현() throws Exception {
-        int requestsPerKey = 1;          // 키당 동시 스레드 수
+        int requestsPerKey = 5;          // 키당 동시 스레드 수
         int durationSeconds = 60;         // 총 테스트 시간 (10초 동안 실행)
         int totalThreads = targetId.size() * requestsPerKey;
 
@@ -116,14 +195,14 @@ public class CacheStampedTest {
         pool.awaitTermination(durationSeconds + 5, TimeUnit.SECONDS);
 
         long cacheMissCounter = itemService.getCacheMissCounter();
-        log.info("cache stampede count : {}", cacheMissCounter);
+        log.info("basic miss count 60s : {}", cacheMissCounter);
 
         Assertions.assertThat(cacheMissCounter).isGreaterThan(0);
     }
 
     @Test
-    void 캐시_스탬프드_지속_재현_Jitter() throws Exception {
-        int requestsPerKey = 1;          // 키당 동시 스레드 수
+    void 캐시_스탬프드_지속_재현_핫키_개선() throws Exception {
+        int requestsPerKey = 5;          // 키당 동시 스레드 수
         int durationSeconds = 60;         // 총 테스트 시간 (10초 동안 실행)
         int totalThreads = targetId.size() * requestsPerKey;
 
@@ -143,7 +222,7 @@ public class CacheStampedTest {
 
                         while (System.currentTimeMillis() < endTime) {
                             try {
-                                mockMvc.perform(get("/item/jitter/{id}", id))
+                                mockMvc.perform(get("/item/hotSafe/{id}", id))
                                         .andExpect(status().isOk());
 
                                 // 1초 대기
@@ -167,7 +246,7 @@ public class CacheStampedTest {
         pool.awaitTermination(durationSeconds + 5, TimeUnit.SECONDS);
 
         long cacheMissCounter = itemService.getCacheMissCounter();
-        log.info("cache stampede count : {}", cacheMissCounter);
+        log.info("lock miss count 60s : {}", cacheMissCounter);
 
         Assertions.assertThat(cacheMissCounter).isGreaterThan(0);
     }
